bfe3cec3345b1b38a6875bfca0f52b94db42b5f1
dthvinh <dangthehienvinh662@gmail.com>
2026-05-17 10:50:32 +0700

    update helm chart, chat server

---
+++ src/main/java/com/prancibot/common/exception/EntityNotFoundException.java
@@ -0,0 +1,8 @@
+package com.prancibot.common.exception;
+
+public class EntityNotFoundException extends RuntimeException {
+
+    public EntityNotFoundException(String msg) {
+        super(msg);
+    }
+}
---
+++ src/main/java/com/prancibot/common/monitoring/annotation/Timed.java
@@ -0,0 +1,14 @@
+package com.prancibot.common.monitoring.annotation;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+/**
+ * Annotation indicating that a method should have its execution time logged
+ */
+@Target(ElementType.METHOD)
+@Retention(RetentionPolicy.RUNTIME)
+public @interface Timed {
+}
